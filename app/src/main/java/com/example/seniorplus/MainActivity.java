3f1bb3642155be881c00105f3ab9a06c07c1fd0b
ZXS <lx094343@gmail.com>
2020-02-16 23:06:52 -0500

    语音与文字双向识别已完成

--- app/src/main/java/com/example/seniorplus/MainActivity.java
+++ app/src/main/java/com/example/seniorplus/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         speechRecognizerBtn = findViewById(R.id.speech_recognizer);
+
         speechRecognizerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,5 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/example/seniorplus/SpeechRecognizerActivity.java
+++ app/src/main/java/com/example/seniorplus/SpeechRecognizerActivity.java
@@ -4,31 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class SpeechRecognizerActivity extends AppCompatActivity {
 
     protected static final int RESULT_SPEECH = 1;
 
-    private Button btnSpeak;
-    private TextView txtText;
+    private Button btnSpeechToText;
+    private TextView txtSpeechToText;
+    private Button btnTextToSpeech;
+    private EditText txtTextToSpeech;
+    TextToSpeech textToSpeech;
+    String sentence = "Hello";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech_recognizer);
 
-        txtText = (TextView) findViewById(R.id.txtText);
-        btnSpeak = (Button) findViewById(R.id.btnSpeak);
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
+        // Speech to text 相关
+        txtSpeechToText = (TextView) findViewById(R.id.txtSpeechToText);
+        btnSpeechToText = (Button) findViewById(R.id.btnSpeechToText);
+        // Text to speech 相关
+        btnTextToSpeech = findViewById(R.id.btnTextToSpeech);
+        txtTextToSpeech = findViewById(R.id.txtTextToSpeech);
+
+        //Speech to Text的逻辑实现
+        btnSpeechToText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //开启语音识别功能
@@ -41,13 +56,42 @@ public class SpeechRecognizerActivity extends AppCompatActivity {
                 //开始识别，这里检测手机是否支持语音识别并且捕获异常
                 try {
                     startActivityForResult(intent, RESULT_SPEECH);
-                    txtText.setText("");
+                    btnSpeechToText.setText("");
                 } catch (ActivityNotFoundException a) {
                     Toast t = Toast.makeText(getApplicationContext(),
                             "Opps! Your device doesn't support Speech to Text",
                             Toast.LENGTH_SHORT);
                     t.show();
                 }
+            }
+        });
+
+
+        //Text to Speech的逻辑实现
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.GERMAN);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        btnTextToSpeech.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+
+        btnTextToSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sentence = txtTextToSpeech.getText().toString();
+                textToSpeech.speak(sentence,TextToSpeech.QUEUE_FLUSH, null);
             }
         });
 
@@ -64,11 +108,24 @@ public class SpeechRecognizerActivity extends AppCompatActivity {
                     ArrayList<String> text = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
-                    txtText.setText(text.get(0));
+                    txtSpeechToText.setText(text.get(0));
                 }
                 break;
             }
 
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+
+
 }

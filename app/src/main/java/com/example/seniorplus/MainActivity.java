4143a95e5a41d6f6eccb9b4b160b471174d03258
ZXS <lx094343@gmail.com>
2020-02-20 20:03:50 -0500

    创建注册界面

--- app/src/main/java/com/example/seniorplus/MainActivity.java
+++ app/src/main/java/com/example/seniorplus/MainActivity.java
@@ -4,17 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     //前往speech recognizer的button
     private Button speechRecognizerBtn;
+    //前往sign up的button
+    private Button btnSighUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         speechRecognizerBtn = findViewById(R.id.speech_recognizer);
+        btnSighUp.findViewById(R.id.btnSignUp);
+
 
         speechRecognizerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,SpeechRecognizerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSighUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });

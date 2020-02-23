b9abc171f288faedc775079bb840ca78cc77df73
Coroline <yawei19970710@outlook.com>
2020-02-23 14:21:36 -0500

    client

--- app/src/main/java/com/example/seniorplus/MainActivity.java
+++ app/src/main/java/com/example/seniorplus/MainActivity.java
@@ -1,34 +1,37 @@
 package com.example.seniorplus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.util.Locale;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     //前往speech recognizer的button
     private Button speechRecognizerBtn;
+    private Button chatRoomBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         speechRecognizerBtn = findViewById(R.id.speech_recognizer);
-
         speechRecognizerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,SpeechRecognizerActivity.class);
                 startActivity(intent);
             }
+        });
+
+        chatRoomBtn = findViewById(R.id.chat_room); chatRoomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ActivityChatMain.class);
+                startActivity(intent);
+           }
         });
 
     }
--- app/src/main/java/util/ImageManager.java
+++ app/src/main/java/util/ImageManager.java
@@ -1,4 +1,23 @@
 package util;
 
+import com.example.seniorplus.R;
+
 public class ImageManager {
+    public static final int[] imagesAvatar = {
+            R.drawable.avasterdr,
+            R.drawable.avasterwe,
+            R.drawable.avasterdandelion,
+            R.drawable.avasteryellow,
+            R.drawable.avastertree,
+            R.drawable.avasterdoor,
+            R.drawable.avastertony
+    };
+    public static final int[] imageID = {
+            R.drawable.msgunselected,
+            R.drawable.contactsunselected,
+            R.drawable.momentunselected,
+            R.drawable.msgselected,
+            R.drawable.contactsselected,
+            R.drawable.momentselected
+    };
 }
